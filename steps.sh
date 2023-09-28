15b84b6fa8670ccb12fa58ebf580ed9dbcaac0e5
0GiS0 <giselatb@outlook.com>
2023-09-28 17:01:03 +0200

    first commit

---
+++ steps.sh
@@ -0,0 +1,5 @@
+# Kubescape
+# ---------------------------------------------------------------------------------
+kubescape scan manifests/*.yaml
+kubescape scan manifests/*.yaml --format json -o results.json
+
