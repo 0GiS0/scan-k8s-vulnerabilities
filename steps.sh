6bba0de7567192f286f6927969bbc073076de87b
Gisela <giselatb@outlook.com>
2023-09-29 19:44:34 +0000

    Add more scan tools

--- steps.sh
+++ steps.sh
@@ -1,5 +1,21 @@
+# kube-score
+# ---------------------------------------------------------------------------------
+kube-score score manifests/*.yaml
+kube-score score manifests/*.yaml --output-format sarif
+
+
 # Kubescape
 # ---------------------------------------------------------------------------------
 kubescape scan manifests/*.yaml
-kubescape scan manifests/*.yaml --format json -o results.json
+kubescape scan manifests/*.yaml --format sarif
 
+# Kubeaudit
+# ---------------------------------------------------------------------------------
+kubeaudit all -f manifests/*.yaml
+kubeaudit autofix -f manifests/*.yaml
+
+# Trivy
+# ---------------------------------------------------------------------------------
+trivy config manifests/
+
+# 
